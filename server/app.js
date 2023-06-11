b0a0e6ec3bc8d19836c4df1c0539fab168c525d7
Roberta <132603728+Bibi0506@users.noreply.github.com>
2023-06-11 18:00:09 +0100

    server done

--- server/app.js
+++ server/app.js
@@ -1,1 +1,30 @@
-// All backend logic
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const geography = require('./geography.json');
+const logger = require("./logger");
+
+app.use(cors());
+app.use(express.json())
+
+//gets the countries-capitals
+app.get(`/`, (req, res) =>{
+    res.send(`Welcome to the quotes API! There are ${geography.length} available.`);
+})
+
+app.get(`/geography`, (req, res)=>{
+    res.send(geography);
+    res.status(200).send(geography);
+})
+
+
+
+app.get(`/geography/random`, (req, res) =>{
+    const randomNumber = Math.floor(Math.random() * geography.length)
+    res.send(geography[randomNumber]);
+    res.status(200).send(geography)
+})
+
+
+
+module.exports = app;
--- server/logger.js
+++ server/logger.js
@@ -1,1 +1,8 @@
 // middleware for logging requests
+function logger(req, res, next) {
+    
+    console.log(req.method, req.originalUrl);
+
+}
+
+module.exports = logger;
