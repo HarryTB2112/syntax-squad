a6f4c6642ecb91ddfb0bcd8079bc57703805fcdc
“harry_tb2112” <harry_tb@icloud.com>
2023-06-12 11:18:03 +0100

    added next() in logger

--- server/app.js
+++ server/app.js
@@ -1,29 +1,29 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const geography = require("./geography.json");
 const logger = require("./logger");
 
+app.use(logger);
 app.use(cors());
 app.use(express.json());
-app.use(logger);
 
 //gets the countries-capitals
 app.get("/", (req, res) => {
-  res.send(
-    `Welcome to the geography API! There are ${geography.length} available.`
-  );
+  res
+    .status(200)
+    .send(
+      `Welcome to the geography API! There are ${geography.length} available.`
+    );
 });
 
 app.get("/geography", (req, res) => {
-  res.send(geography);
   res.status(200).send(geography);
 });
 
 app.get("/geography/random", (req, res) => {
   const randomNumber = Math.floor(Math.random() * geography.length);
-  res.send(geography[randomNumber]);
-  res.status(200).send(geography);
+  res.status(200).send(geography[randomNumber]);
 });
 
 module.exports = app;
--- server/logger.js
+++ server/logger.js
@@ -1,8 +1,7 @@
 // middleware for logging requests
 function logger(req, res, next) {
-    
-    console.log(req.method, req.originalUrl);
-
+  console.log(req.method, req.originalUrl);
+  next();
 }
 
 module.exports = logger;
