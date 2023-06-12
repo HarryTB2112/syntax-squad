6daa3b2c3b14cdd398ff96fea7a2700c80620aa9
Modernsapien <danielscott907@yahoo.com>
2023-06-12 17:32:16 +0100

    updated  checkLoss function

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ const letters = document.querySelectorAll("#letters");
 const displayLetter = document.getElementById("display-letter");
 let countryLetters = [];
 let capitalLetters = [];
-const alphabet='abcdefghijklmnopqrstuvwxyz'.split ('') 
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
 //event listeners
 
@@ -37,64 +37,90 @@ function openHangman() {
   fetchApiData();
 }
 
-function displayCountry(){
-    const underscoreContainer = document.getElementById("underscoreContainer");
-    underscoreContainer = "";
-        for (let i = 0; i < country.length; i++) {
-        const underscore = document.createElement("p");
-        underscore.textContent = "_";
-        underscoreContainer.appendChild(underscore);
-      }}
+function displayCountry() {
+  const underscoreContainer = document.getElementById("underscore-container");
+  underscoreContainer.innerHTML = "";
+  for (let i = 0; i < countryLetters.length; i++) {
+    const underscore = document.createElement("p");
+    underscore.textContent = "_";
+    underscoreContainer.appendChild(underscore);
+  }
+}
+
+function displayCapital() {
+  const underscoreContainer = document.getElementById("underscore-container");
+  underscoreContainer.innerHTML = "";
+  for (let i = 0; i < capitalLetters.length; i++) {
+    const underscore = document.createElement("p");
+    underscore.textContent = "_";
+    underscoreContainer.appendChild(underscore);
+  }
+}
 
 // get country & city name and turn them into each letter
 function addApi(data) {
   const country = data.country;
   const capital = data.capital;
-  countryLetters = country.split("");
-  capitalLetters = capital.split("");
-  displayWord();
+  countryLetters = country.toUpperCase().split("");
+  capitalLetters = capital.toUpperCase().split("");
+  displayCapital();
+  displayCountry()
 }
 
-
 function checkLetter(country, capital) {
-  const clickedLetter = event.target.textContent;
+  const clickedLetter = event.target.textContent.toUpperCase();
+  let letterFound = false;
   for (let i = 0; i < countryLetters.length; i++) {
     if (countryLetters[i] === clickedLetter) {
       countryLetters.splice(i, 1);
       displayLetter.textContent += clickedLetter;
-      checkWin()
+      letterFound = true;
+      checkWin();
+    break;
+
     }
   }
   for (let i = 0; i < capitalLetters.length; i++) {
     if (capitalLetters[i] === clickedLetter) {
-        countryLetters.splice(i, 1);
-        displayLetter.textContent += capitalLetters[i];
-        checkWin()
+      countryLetters.splice(i, 1);
+      displayLetter.textContent += capitalLetters[i];
+     letterFound=true
+     checkWin();
+    break;
     }
+    
   }
+  if (!letterFound){
+    checkLoss()
 }
+}
+
+
 
 function checkWin() {
-    if (countryLetters.length === 0 && capitalLetters.length === 0) {
-  alert("You won!");
-    }
-  }
-
-/// this function needs to be fixed
-function checkLoss(){
-  //define bodyparts (they are urls)
-  //conditional statement when body parts are all gone, give an alert to try again
-  const img=""
-  arrayImages=["Hangman1.jpg","Hangman2.jpg", "Hangman3.jpg", "Hangman4.jpg", 
-  "Hangman5.jpg", "Hangman6.jpg", "Hangman7.jpg"]
-  
-  for (let j=0; j<arrayImages.length; j++){
-    if (img===arrayImages[6]){
-      alert("Try Again!")
-    }
+  if (countryLetters.length === 0 && capitalLetters.length === 0) {
+    alert("You won!");
   }
 }
 
-  // Update the HTML with the new word display
-  wordDisplayElement.textContent = wordDisplay;
+/// this function needs to be fixed
+function checkLoss() {
+  //define bodyparts (they are urls)
+  //conditional statement when body parts are all gone, give an alert to try again
+  const img = "";
+  const arrayImages = [
+    "Hangman1.jpg",
+    "Hangman2.jpg",
+    "Hangman3.jpg",
+    "Hangman4.jpg",
+    "Hangman5.jpg",
+    "Hangman6.jpg",
+    "Hangman7.jpg",
+  ];
 
+  for (let j = 0; j < arrayImages.length; j++) {
+    if (img === arrayImages[6]) {
+      alert("Try Again!");
+    }
+  }
+}
