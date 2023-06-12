927e96a49a00133b86886b806437a041c53f25bd
Modernsapien <danielscott907@yahoo.com>
2023-06-12 10:10:39 +0100

    Co-authored-by: Harry Turner-Burns <HarryTB2112@users.noreply.github.com> Co-authored-by: Roberta <Bibi0506@users.noreply.github.com>

--- client/index.js
+++ client/index.js
@@ -1,1 +1,10 @@
 // HAngman functionality
+// Game variables
+var wordToGuess = "hangman"; // The word to guess
+var lettersGuessed = []; // Array to store the letters already guessed
+
+//Play game
+//function to initialise the game
+
+
+//function to update the word displayed
